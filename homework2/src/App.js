674cf9877a734ff47516241d40ecb2e87af7bb67
omargaziev.d9 <omargaziev.d9@gmail.com>
2023-10-01 16:48:23 +0600

    Homework3 HOC, Hooks

--- homework2/src/App.js
+++ homework2/src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React, { useState, createContext, useContext } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Компонент для добавления людей
+const AddPersonForm = () => {
+    const { addPerson } = usePeople();
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
 
-export default App;
+    const handleAddPerson = () => {
+        if (name && age) {
+            console.log(`Adding person: Name - ${name}, Age - ${age}`);
+            addPerson({ name, age });
+            setName('');
+            setAge('');
+        }
+    };
+
+    return (
+        <div>
+            <h2>Add a Person</h2>
+            <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input
+                type="number"
+                placeholder="Age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+            />
+            <button onClick={handleAddPerson}>Add Person</button>
+        </div>
+    );
+};
+
+// HOC для управления списком людей
+const PeopleContext = createContext();
+
+export const usePeople = () => {
+    return useContext(PeopleContext);
+};
+
+const withPeople = (WrappedComponent) => {
+    return (props) => {
+        const [people, setPeople] = useState([]);
+
+        const addPerson = (person) => {
+            setPeople([...people, person]);
+        };
+
+        const removePerson = (index) => {
+            const newPeople = [...people];
+            newPeople.splice(index, 1);
+            setPeople(newPeople);
+        };
+
+        return (
+            <PeopleContext.Provider value={{ people, addPerson, removePerson }}>
+                <WrappedComponent {...props} />
+            </PeopleContext.Provider>
+        );
+    };
+};
+
+// Компонент для отображения списка людей
+const PeopleList = () => {
+    const { people, removePerson } = usePeople();
+
+    return (
+        <div>
+            <h2>People List</h2>
+            <ul>
+                {people.map((person, index) => (
+                    <li key={index}>
+                        {person.name}, {person.age} years old{' '}
+                        <button onClick={() => removePerson(index)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+// Основной компонент приложения
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Add People to the System</h1>
+            <AddPersonForm />
+            <PeopleList />
+        </div>
+    );
+};
+
+export default withPeople(App);
